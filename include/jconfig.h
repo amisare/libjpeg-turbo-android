38f0ee54fc85ed2e966dc7e3a8466ff2279500d5
Ailv <243297288@qq.com>
2023-06-26 14:00:06 +0800

    + add libjpeg-turbo 2.1.x

--- include/jconfig.h
+++ include/jconfig.h
@@ -4,10 +4,10 @@
 #define JPEG_LIB_VERSION  62
 
 /* libjpeg-turbo version */
-#define LIBJPEG_TURBO_VERSION  2.1.5.1
+#define LIBJPEG_TURBO_VERSION  2.1.6
 
 /* libjpeg-turbo version in integer form */
-#define LIBJPEG_TURBO_VERSION_NUMBER  2001005
+#define LIBJPEG_TURBO_VERSION_NUMBER  2001006
 
 /* Support arithmetic encoding */
 #define C_ARITH_CODING_SUPPORTED 1
--- include/jconfigint.h
+++ include/jconfigint.h
@@ -14,7 +14,7 @@
 #define PACKAGE_NAME  "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION  "2.1.5.1"
+#define VERSION  "2.1.6"
 
 /* The size of `size_t', as computed by sizeof. */
 // #define SIZEOF_SIZE_T  8
