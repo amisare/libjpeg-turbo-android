fbf79af5841a544d095daf56d396a19fdc84fb79
Ailv <243297288@qq.com>
2023-06-26 10:09:34 +0800

    + add libjpeg-turbo 2.0.0

---
+++ toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER /Users/guhaijun/Library/Android/sdk/ndk/16.1.4479499/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin/aarch64-linux-android-gcc)
+set(CMAKE_FIND_ROOT_PATH /Users/guhaijun/Library/Android/sdk/ndk/16.1.4479499/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/aarch64-linux-android)
