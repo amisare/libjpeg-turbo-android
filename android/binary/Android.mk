ae9a413e5f6e4732060ab997e3b5c5c6b9743bff
Ailv <243297288@qq.com>
2023-06-29 16:44:21 +0800

    ~ 修改 binary Android.mk

--- android/binary/Android.mk
+++ android/binary/Android.mk
@@ -10,7 +10,7 @@ LIBJPEG_TURBO_LIBS_DIR := lib/$(TARGET_ARCH_ABI)
 include $(CLEAR_VARS)
 LOCAL_MODULE := jpeg_static
 LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
-LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libjpeg.a
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libjpeg_static.a
 ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
     include $(PREBUILT_STATIC_LIBRARY)
 endif
@@ -26,7 +26,7 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE := turbojpeg_static
 LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
-LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libturbojpeg.a
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libturbojpeg_static.a
 ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
     include $(PREBUILT_STATIC_LIBRARY)
 endif
@@ -37,4 +37,36 @@ LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
 LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libturbojpeg.so
 ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
     include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := jpeg12_static
+LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libjpeg12_static.a
+ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
+    include $(PREBUILT_STATIC_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := jpeg16_static
+LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libjpeg16_static.a
+ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
+    include $(PREBUILT_STATIC_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := turbojpeg12_static
+LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libturbojpeg12_static.a
+ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
+    include $(PREBUILT_STATIC_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := turbojpeg16_static
+LOCAL_EXPORT_C_INCLUDES := $(LIBJPEG_TURBO_C_INCLUDES_DIR)
+LOCAL_SRC_FILES := $(LIBJPEG_TURBO_LIBS_DIR)/libturbojpeg16_static.a
+ifeq ($(filter $(modules-get-list), $(LOCAL_MODULE)),)
+    include $(PREBUILT_STATIC_LIBRARY)
 endif
